8f5aad8a7270b19d6ee347e793d52179cd21ea18
William <wnmerukini@gmail.com>
2021-07-17 22:25:51 +0300

    transition speed

--- Player/Main Menu/MainMenu.cs
+++ Player/Main Menu/MainMenu.cs
@@ -54,7 +54,7 @@ namespace ChampionsOfForest
 		private GUIStyle menuBtnStyle;                  //style of font for main menu button
 		private GUIStyle chgDiffLabelStyle;
 		private GUIStyle chgDiffBtnStyle;
-		private const float screenTransisionSpeed = 20;      //speed of transion between menus
+		private const float screenTransisionSpeed = 13;      //speed of transion between menus
 		public Font mainFont;                           //main font for the mod
 		public Font secondaryFont;                      //secondary font
 
