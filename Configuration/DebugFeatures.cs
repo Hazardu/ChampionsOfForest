81c9c6361ce1356ab47286cb0c8e861d0f02fce9
William <wnmerukini@gmail.com>
2021-07-18 19:46:32 +0300

    Simple Ruler

--- Configuration/DebugFeatures.cs
+++ Configuration/DebugFeatures.cs
@@ -1,19 +1,84 @@
 ﻿using ChampionsOfForest.Player;
 
+using TheForest.Utils;
+
 using UnityEngine;
+
+using Input = UnityEngine.Input;
 
 namespace ChampionsOfForest
 {
 	//implement additional UI elements
 	public partial class MainMenu : MonoBehaviour
 	{
-#if DEBUG
+#if Debugging_Enabled
 
 		public static Vector3 _DEBUG_WEAPON_POSITION_OFFSET, _DEBUG_WEAPON_ROTATION_OFFSET;
 		string posX= "0.0", posY= "0.0", posZ= "0.0";
 		string rotX= "0.0", rotY= "0.0", rotZ= "0.0";
+		private Vector3 rulerStart = Vector3.zero;
+		private Vector3 rulerEnd = Vector3.zero;
+		private int rulerHue = 1;
+		private GameObject rulerObj;
 		partial void DrawDebug()
 		{
+			if (rulerObj == null)
+			{
+				rulerObj = new GameObject();
+				rulerObj.AddComponent<LineRenderer>();
+			}
+			GUI.Label(new Rect(10, 10, 500, 30), $"Player position X = {LocalPlayer.Transform.position.x} Y = {LocalPlayer.Transform.position.y} Z = {LocalPlayer.Transform.position.z}");
+			//Ruler
+			if (Input.GetKeyDown(KeyCode.F7))
+			{
+				rulerStart = LocalPlayer.Transform.position;
+			}
+			if (Input.GetKeyDown(KeyCode.F8))
+			{
+				rulerEnd = LocalPlayer.Transform.position;
+			}
+			if (Input.GetKeyDown(KeyCode.F9))
+			{
+				rulerStart = Vector3.zero;
+				rulerEnd = Vector3.zero;
+			}
+			if (rulerStart != Vector3.zero && rulerEnd != Vector3.zero)
+			{
+				rulerHue++;
+				Color rulerColor = Color.HSVToRGB((rulerHue % 360) / 360f, 1f, 1f);
+				rulerObj.transform.position = rulerStart;
+				
+				LineRenderer lr = rulerObj.GetComponent<LineRenderer>();
+				lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+				lr.SetColors(rulerColor, rulerColor);
+				lr.SetWidth(0.1f, 0.1f);
+				lr.SetPosition(0, rulerStart);
+				lr.SetPosition(1, rulerEnd);
+				Vector3 firstCameraPoint = Camera.main.WorldToScreenPoint(rulerStart);
+				Vector3 lastCameraPoint = Camera.main.WorldToScreenPoint(rulerEnd);
+				if (firstCameraPoint.z > 0f)
+				{
+					firstCameraPoint.y = Screen.height - firstCameraPoint.y;
+					float size = Mathf.Clamp(600 / Vector3.Distance(rulerStart, LocalPlayer.Transform.position), 10, 50);
+					size *= screenScale;
+					Rect r = new Rect(0, 0, 400, size)
+					{
+						center = firstCameraPoint
+					};
+					GUI.Label(r, $"Client distance = {Vector3.Distance(rulerStart, LocalPlayer.Transform.position)}");
+				}
+				if (lastCameraPoint.z > 0f)
+				{
+					lastCameraPoint.y = Screen.height - lastCameraPoint.y;
+					float size = Mathf.Clamp(600 / Vector3.Distance(rulerEnd, LocalPlayer.Transform.position), 10, 50);
+					size *= screenScale;
+					Rect r = new Rect(0, 0, 400, size)
+					{
+						center = lastCameraPoint
+					};
+					GUI.Label(r, $"Client distance = {Vector3.Distance(rulerEnd, LocalPlayer.Transform.position)}\nLineLen = {Vector3.Distance(rulerStart, rulerEnd)}");
+				}
+			}
 			if (PlayerInventoryMod.EquippedModel != BaseItem.WeaponModelType.None)
 			{
 				GUI.Label(new Rect(600, 300, 500, 30), "Offset");
